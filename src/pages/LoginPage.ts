c1defff594deff78a2ffb23b2540e2d60377e539
Vijetha Gopisetty <vijetha.gopisetty@gmail.com>
2026-09-14 20:49:33 -0700

    check self heal in ci with latest

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -35,7 +35,7 @@ export class LoginPage extends BasePage {
 
   constructor(page: Page) {
     super(page);
-    this.usernameInput = page.locator('input[name="username"]');
+    this.usernameInput = page.locator('input[name="user name"]');
     this.passwordInput = page.locator('input[name="password"]');
     this.loginButton = page.getByRole('button', { name: 'Log In' });
 
